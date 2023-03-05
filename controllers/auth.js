341d71ecb8682aeb93fee22ac404541960450f20
Walter Daniel <walterdcarrizo.19@gmail.com>
2023-03-05 13:08:17 -0300

    Esquemas de de usuario, instalación de bcrypt para encriptar las contraseñas

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,31 +1,79 @@
+const User = require('../schemas/UserSchema');
+const bcrypt = require('bcryptjs');
 
+const register = async(req, res) => {
 
-const register = (req, res) => {
+    const {  email, password } = req.body;
+      
+    try {
 
-    const { name, surname, email, password } = req.body;
+        let emailExist = await User.findOne({ email });
 
-    
-    res.status(201).json({
-        ok: true,
-        msg: 'Usuario creado con éxito',
-        name,
-        surname,
-        email,
-        password
-    })
+        if( emailExist ) {
+            return res.status(400).json({
+                ok: false,
+                msg: 'Ya existe un usuario con ese correo'
+            })
+        }
+
+        const newUser = new User( req.body );
+        const salt = bcrypt.genSaltSync();
+        newUser.password = bcrypt.hashSync( password, salt );
+        await newUser.save();
+
+        res.status(201).json({
+            ok: true,
+            msg: 'Usuario creado con éxito',
+            newUser
+        });
+    } catch (error) {
+        res.status(500).json({
+            ok: false,
+            msg: 'Error al crear un nuevo usuario'
+        });
+    }
 }
 
-const login = (req, res) => {
+const login = async(req, res) => {
 
     const { email, password } = req.body;
 
+    try {
 
-    res.status(201).json({
-        ok: true,
-        msg: 'Inicio de sesión exitoso',
-        email,
-        password
-    })
+       const userExist = await User.findOne({ email });
+
+        if( !userExist ) {
+            return res.status(500).json({
+                ok: false,
+                msg: 'Las credenciales no son correctas'
+            });
+        }
+
+        const passwordValid = bcrypt.compareSync( password, userExist.password );
+
+        if(!passwordValid){
+            return res.status(400).json ({
+                ok:false,
+                msg: 'Credenciales incorrectas'
+            })
+        }
+
+        res.status(201).json({
+            ok: true,
+            msg: 'Inicio de sesión exitoso',
+            id: userExist.id,
+            name: userExist.name
+        });
+
+
+    } catch (error) {
+        res.status(500).json({
+            ok: false,
+            msg: 'Error al iniciar sesión'
+        });
+    }
+
+    
 }
 
 const revalidateToken = (req, res) => {
